3d1f99d2967d220cdfcc568209f0f691d3190975
Mera44 <Mera@meras-MacBook-Pro.local>
2017-10-27 16:10:26 -0500

    first commit

---
+++ src/main/java/mum/edu/MumSchedApplication.java
@@ -0,0 +1,12 @@
+package mum.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MumSchedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MumSchedApplication.class, args);
+	}
+}
